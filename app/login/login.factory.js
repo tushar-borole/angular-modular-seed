99895aa55db3c3999d33369d798a7db7da0cc6e6
Tushar Borole <tushariscoolster@gmail.com>
2015-04-24 21:15:01 +0530

    Login factory demo

--- app/login/login.factory.js
+++ app/login/login.factory.js
@@ -1,8 +1,24 @@
-(function() {
+(function () {
     'use strict';
-angular
-    .module
-    .factory('login', login);
+    angular
+        .module
+        .factory('login', login);
 
-function login() { }
-    })();
+    function login() {
+        var loginFactory = {};
+
+
+        /**
+         * @description [[ Function to make GetAllCenter API]]
+         * @name centFactory.get
+         * @param  {Object} data [[data contain Offset,Limit,Role,search keywords]]
+         */
+        loginFactory.get = function (data) {
+            var url = APP_URL[$enviornment.urlname].getcenter;
+            var postType = restangularParams('post', $enviornment.urlname);
+            return Restangular[postType.value](url)[postType.type](data);
+        };
+
+
+    }
+})();
