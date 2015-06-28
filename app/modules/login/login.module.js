0e59a9cf11cbc5be1eebae34027f231ada055c5d
Tushar Borole <tushariscoolster@gmail.com>
2015-06-29 02:59:59 +0530

    Seed Changes

--- app/modules/login/login.module.js
+++
@@ -1,8 +0,0 @@
-angular.module('AeroD', ['AeroD.login', {
-    files: [
-        'modules/login/login.controller.js',
-        'modules/login/login.factory.js',
-        'common/directive/sweetalert.directive.js'
-    ],
-    cache: false
-}])
